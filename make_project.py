d2c09fbc9ffe3b775744ec6b279f1cbfd36a9b3e
Justina <justcho5@gmail.com>
2019-11-21 14:51:26 -0600

    changes

--- make_project.py
+++ make_project.py
@@ -21,31 +21,31 @@ import random
 def sample(dir_list, num_random_images):
     tiles = []
 
-    if len(tcga_dirs)>num_random_images:
-        sample = random.sample(tcga_dirs, num_random_images)
-        for dir in tcga_dirs:
-            sample_dir = os.path.join(tiles_dir, dir)
+    if len(dir_list)>num_random_images:
+        sample = random.sample(dir_list, num_random_images)
+        for dir in dir_list:
+            sample_dir = os.path.join(tiles_nolabel_dir, dir)
             for root, _, files in os.walk(sample_dir):
                 tiles.append(os.path.join(root,random.choice(files)))
                 break
     else:
         sample = []
-        sample.append(random.sample(tcga_dirs, r))
-        for dir in tcga_dirs:
-            sample_dir = os.path.join(tiles_dir, dir)
+        sample.append(random.sample(dir_list, r))
+        for dir in dir_list:
+            sample_dir = os.path.join(tiles_nolabel_dir, dir)
             for _,_,files in os.walk(sample_dir):
                 samples = random.sample(files, q)
                 for sample in samples:
                     tiles.append(os.path.join(root,sample))
                 break
         for dir in sample:
-            sample_dir = os.path.join(tiles_dir, dir)
+            sample_dir = os.path.join(tiles_nolabel_dir, dir)
             for _,_,files in os.walk(sample_dir):
                 tiles.append(os.path.join(root,random.choice(files)))
                 break
     return tiles
 
-def copy_rand_images(tiles_dir, num_random_images, annota_path = None, category = None):
+def copy_rand_images(tiles_nolabel_dir, num_random_images, annota_path = None, category = None):
 
     if category:
         df = pd.read_csv("annota_path")
@@ -55,7 +55,7 @@ def copy_rand_images(tiles_dir, num_random_images, annota_path = None, category 
 
 
     else:
-        for root, dirs, files in os.walk(tiles_dir):
+        for root, dirs, files in os.walk(tiles_nolabel_dir):
             tcga_dirs = [dir for dir in dirs if "TCGA" in dir]
             dic = {}
             q = num_random_images/len(tcga_dirs)
@@ -68,6 +68,7 @@ def copy_rand_images(tiles_dir, num_random_images, annota_path = None, category 
 def main():
     project_name = "hpv_224"
     tiles_dir = "/mnt/gpucluster/hnsc/slideflow_projects/hpv_224/tiles/"
+    tiles_nolabel_dir = os.path.join(tiles_dir, "no_label")
     num_expts = 50
     num_random_images = 100
     project_dir = os.path.join(tiles_dir, project_name)
@@ -84,13 +85,13 @@ def main():
     for i in range(num_expts+1):
         experiment_dir = os.path.join(source_dir, "random500_{}".format(i))
         tf.gfile.MakeDirs(experiment_dir)
-        tiles = copy_rand_images(tiles_dir, num_random_images)
+        tiles = copy_rand_images(tiles_nolabel_dir, num_random_images)
         for tile in tiles:
             tf.gfile.Copy(tile, experiment_dir)
         if i ==num_expts:
             tf.gfile.Rename(experiment_dir, random_discovery)
     for category in ["positive", "negative"]:
-        tiles = copy_rand_images(tiles_dir, num_random_images, annota_path="/mnt/gpucluster/hnsc/slideflow_projects/hpv_224/tcga_hnsc_anns_n_slides.csv", category=category)
+        tiles = copy_rand_images(tiles_nolabel_dir, num_random_images, annota_path="/mnt/gpucluster/hnsc/slideflow_projects/hpv_224/tcga_hnsc_anns_n_slides.csv", category=category)
         if category == "positive":
             for tile in tiles:
                 tf.gfile.Copy(tile, pos_dir)
